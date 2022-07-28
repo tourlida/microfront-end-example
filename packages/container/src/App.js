8ec390dd41c1c45d7772ff9217234b3c2ec68ae2
tourlida <vagiatourlida@hotmail.com>
2022-07-28 12:41:32 +0300

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+export default () => {
+  return (
+    <div>
+      hi there!
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+};
