2208583378bdc3bea5765067a41e835444647f39
tourlida <vagiatourlida@hotmail.com>
2022-07-30 09:57:12 +0300

    randomize production classNames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,25 @@
 import React from "react";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
+//use it to solve collision of the css in the production
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
-import Landing from './components/Landing';
-import Pricing from './components/Pricing';
+import Landing from "./components/Landing";
+import Pricing from "./components/Pricing";
+
+//use it to solve collision of the css in the production
+const generateClassName = createGenerateClassName({
+  productionPrefix: "ma",
+});
 
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
